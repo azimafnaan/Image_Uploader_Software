4429014860f2f3a1a022ffbed7bd72bd2a3a46a1
azimafnaan <azim.afnaan.hotmail.com>
2021-02-09 02:48:28 +0600

    first commit

---
+++ photos/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from .models import *
+
+# Create your views here.
+
+def gallery(request):
+    category = request.GET.get('category')
+    if category == None:
+        photos=Photo.objects.all() 
+    else:
+        photos = Photo.objects.filter(category__name=category)
+
+    categories = Category.objects.all()
+    context= {'categories': categories, 'photos': photos}
+    return render(request, 'photos/gallery.html', context)
+
+def viewphoto(request, pk):
+    photo = Photo.objects.get(id=pk)
+    return render(request, 'photos/photo.html', {'photo': photo})
+
+def addphoto(request):
+    categories=Category.objects.all()
+
+    if request.method == 'POST':
+        data = request.POST
+        image = request.FILES.get('image')
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['category_new'] !='':
+            category, created= Category.objects.get_or_create(name=data['category_new'])
+        else:
+            category = None
+        photo = Photo.objects.create(
+            category=category,
+            image=image,
+        ) 
+        return redirect('gallery')            
+      
+
+    context= {'categories': categories}
+    return render(request, 'photos/add.html', context)
+    
